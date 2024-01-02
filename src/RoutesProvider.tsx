e8b286b35f4d5030663af5db49b6734050a00796
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-02 16:33:05 +0100

    fixed @ imports

--- src/RoutesProvider.tsx
+++ src/RoutesProvider.tsx
@@ -2,7 +2,7 @@ import { basename } from '@tauri-apps/api/path';
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from './App';
-import Dashboard from './pages/Dashboard';
+import Dashboard from '@pages/Dashboard';
 
 
 export default function RoutesProvider() {
