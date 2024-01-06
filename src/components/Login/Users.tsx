47fcb39819096ca64a101916578bf23db06f45af
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-06 13:36:08 +0100

    wip with users component

---
+++ src/components/Login/Users.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "/resources/styles/components/login/users.scss";
+import UserItem from './UserItem';
+
+type UsersProps = {
+  users: any[],
+  loginActive: boolean,
+  setLoginActive: (loginActive: boolean) => void,
+}
+
+export default function Users(usersProps: UsersProps) {
+  const { users, loginActive, setLoginActive } = usersProps;
+
+  const testUser = {
+    id: 1,
+    username: 'test',
+    profilePicture: 'default',
+    createdAt: '2021-08-01T00:00:00.000Z',
+    updatedAt: '2021-08-01T00:00:00.000Z',
+  } as any;
+
+  const handleUserClick = (user: any) => {
+    console.log(user);
+    setLoginActive(true);
+  }
+  return (
+    <div className='login-users-container'>
+
+      <UserItem user={testUser} onClick={handleUserClick} />
+    </div>
+  )
+}
