8b7d8c5b090922d1423f5467b8c2381da9866800
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-10 16:46:52 +0100

    now fetching all users to show them

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "/resources/styles/pages/login.scss";
 import logo from "/resources/images/logo.svg";
 import Users from "@components/Login/Users";
 import LoginForm from "@components/Login/LoginForm";
 import Footer from "@/components/Login/Footer";
+import { set } from "react-hook-form";
 
 export default function Login() {
   const [loginActive, setLoginActive] = useState(false);
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const data = await (await fetch("/api/auth/users")).json();
+    return data;
+  };
 
+  useEffect(() => {
+    (async () => {
+      setUsers(await getUsers());
+    })()
+  }, []);
   return (
     <div className="login-container">
       <img className="logo-absolute" src={logo} alt="Barbiekoe" />
@@ -15,7 +26,7 @@ export default function Login() {
         <Users
           loginActive={loginActive}
           setLoginActive={setLoginActive}
-          users={[]}
+          users={users}
         />
         <LoginForm />
       </div>
