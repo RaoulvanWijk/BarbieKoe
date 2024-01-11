fc12b5feeb2e67815fd31bcc65e983dc32bc7ef7
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-11 16:30:37 +0100

    refactored the code so its more readable

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,32 +4,33 @@ import logo from "/resources/images/logo.svg";
 import Users from "@components/Login/Users";
 import LoginForm from "@components/Login/LoginForm";
 import Footer from "@/components/Login/Footer";
-import { set } from "react-hook-form";
+import { SafeUser as User } from "@lib/types/database";
+
+import { useData } from "@/lib/hooks/fetch";
 
 export default function Login() {
-  const [loginActive, setLoginActive] = useState(false);
-  const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState({
-    id: null,
+  const tempUser: User = {
+    id: -1,
     username: "No user selected",
-    profilePicture: null,
-    createdAt: "2021-08-01T00:00:00.000Z",
-    updatedAt: "2021-08-01T00:00:00.000Z",
-  } as any);
-  const getUsers = async () => {
-    const data = await (await fetch("/api/auth/users")).json();
-    return data;
+    profile_picture: null,
+    created_at: new Date("2024-01-11 16:06:36"),
+    updated_at: new Date("2024-01-11 16:06:36"),
   };
 
-  useEffect(() => {
-    (async () => {
-      setUsers(await getUsers());
-    })()
-  }, []);
+  const [loginActive, setLoginActive] = useState<boolean>(false);
+  const users = useData("/api/auth/users") as User[];
+  const [selectedUser, setSelectedUser] = useState<User>(tempUser);
+  
+  if(!users) return <div>Loading...</div>;
+
   return (
     <div className="login-container">
       <img className="logo-absolute" src={logo} alt="Barbiekoe" />
-      <div className={"login-content " + (loginActive ? "login-active" : "user-active")}>
+      <div
+        className={
+          "login-content " + (loginActive ? "login-active" : "user-active")
+        }
+      >
         <Users
           loginActive={loginActive}
           setLoginActive={setLoginActive}
