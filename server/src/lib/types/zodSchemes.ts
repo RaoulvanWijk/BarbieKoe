be3cc1b4941ffd14768a84e25b4ef28296d8a776
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-11 21:07:50 +0100

    attempt to remove it from git

--- server/src/lib/types/zodSchemes.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-  password: z.string().min(6, { message: "Wachtwoord is minimaal 6 karakters lang" }),
-});
-
-export type TLoginSchema = z.infer<typeof loginSchema>;
