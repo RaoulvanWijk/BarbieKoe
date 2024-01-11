e1a130321c80f613077c12a75fe30fa18aa8f529
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-11 16:30:20 +0100

    added + fixed some database types

--- src/lib/types/database.ts
+++ src/lib/types/database.ts
@@ -1,16 +1,34 @@
 export type SafeUser = {
   id: number,
   username: string,
-  profilePicture: string|null,
-  createdAt: Date,
-  updatedAt: Date,
+  profile_picture: string|null,
+  created_at: Date,
+  updated_at: Date,
 }
 
 export type User = {
   id: number,
   username: string,
   password: string,
-  profilePicture: string|null,
+  profile_picture: string|null,
+  created_at: Date,
+  updated_at: Date,
+}
+
+export type Session = {
+  id: number,
+  userId: number,
+  token: string,
   createdAt: Date,
   updatedAt: Date,
 }
+
+export type UserSession = {
+  id: number,
+  username: string,
+  profile_picture: string|null,
+  created_at: Date,
+  updated_at: Date,
+  token: string,
+}
+
