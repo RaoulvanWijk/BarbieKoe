4807d125224221b6e28dacb7343c5de60c1d945b
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-11 17:16:46 +0100

    wip with login api

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -8,4 +8,12 @@ router.get('/users', async (req, res) => {
   res.status(200).send(resu[0]);
 });
 
+router.post('/login', async (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  // const resu = await query("CALL sp_login(?, ?)", [username, password]) as any;
+  res.status(200).send();
+});
+
 module.exports = router;
