4192c9664b392300ff61e7c11bcc8dc7f654d2de
hlwalZ <ajustytmy@gmail.com>
2024-01-15 00:15:55 +0100

    Booking API alleen werkend voor creëren van: Reserverings formulier posten naar database.

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,8 +9,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
+app.use(Express.json());
 // load routes from auth folder
 app.use('/auth', require('./routes/auth'));
+app.use("/booking", require("./routes/booking"));
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
