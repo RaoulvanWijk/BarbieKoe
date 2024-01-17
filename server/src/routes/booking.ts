29b39c6c2d12a782cf14109bed6b46237377d456
hlwalZ <ajustytmy@gmail.com>
2024-01-17 12:28:24 +0100

    Meerdere statements mogelijk gemaakt in db.ts, queries gebruiken nu NOW functie en wanneer toepasselijk LAST_INSERT_ID functie. createBooking endpoint verbeterd door een transactie te maken en gebruik te maken van meerdere statements in één query i.p.v. vier

--- server/src/routes/booking.ts
+++ server/src/routes/booking.ts
@@ -7,8 +7,7 @@ const router = Router();
 
 router.post("/createBooking", async (req, res) => {
   console.log(req.body);
-  const created_at = new Date().toISOString().slice(0, 19).replace("T", " ");
-  const updated_at = created_at;
+
   const {
     house_number,
     city,
@@ -31,25 +30,32 @@ router.post("/createBooking", async (req, res) => {
     camping_spot_id,
   } = req.body;
 
-  const createAddress: any = await query(
-    "INSERT INTO address (house_number, city, country, streetname, zipcode, created_at, updated_at) values (?,?,?,?,?,?,?)",
-    [house_number, city, country, streetname, zipcode, created_at, updated_at]
-  );
-
-  const address_id = createAddress.insertId;
-
-  const createGuests: any = await query(
-    "INSERT INTO guests (address_id, first_name, last_name, phone, email, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
-    [address_id, first_name, last_name, phone, email, created_at, updated_at]
-  );
-
-  const guest_id = createGuests.insertId;
   const cost = adult * 20 + child * 10 + young_child * 5;
 
-  const createBooking: any = await query(
-    "INSERT INTO booking (guest_id, camping_spot_id, arrival, departure, adult, child, young_child, cost, booking_status, notes, created_at, updated_at) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)",
+  await query(
+    `
+  START TRANSACTION;
+
+  INSERT INTO address (house_number, city, country, streetname, zipcode, created_at, updated_at) VALUES (?,?,?,?,?,NOW(),NOW());
+
+  INSERT INTO guests (address_id, first_name, last_name, phone, email, created_at, updated_at) VALUES (LAST_INSERT_ID(), ?, ?, ?, ?, NOW(), NOW());
+
+  INSERT INTO booking (guest_id, camping_spot_id, arrival, departure, adult, child, young_child, cost, booking_status, notes, created_at, updated_at) VALUES (LAST_INSERT_ID(),?,?,?,?,?,?,?,?,?,NOW(),NOW());
+
+  INSERT INTO cars (booking_id, license_plate, car_status, created_at, updated_at) VALUES (LAST_INSERT_ID(),?,?,NOW(),NOW());
+
+  COMMIT;
+`,
     [
-      guest_id,
+      house_number,
+      city,
+      country,
+      streetname,
+      zipcode,
+      first_name,
+      last_name,
+      phone,
+      email,
       camping_spot_id,
       arrival,
       departure,
@@ -59,55 +65,42 @@ router.post("/createBooking", async (req, res) => {
       cost,
       booking_status,
       notes,
-      created_at,
-      updated_at,
+      license_plate,
+      car_status,
     ]
-  );
-
-  const booking_id = createBooking.insertId;
-
-  await query(
-    "INSERT INTO cars (booking_id, license_plate, car_status, created_at, updated_at) VALUES(?,?,?,?,?)",
-    [booking_id, license_plate, car_status, created_at, updated_at]
   );
 
   res.status(200).send(`Succesvol een nieuw form aangemaakt.`);
 });
 
 router.post("/createCostGuest", async (req, res) => {
-  const created_at = new Date().toISOString().slice(0, 19).replace("T", " ");
-  const updated_at = created_at;
   const { person_type, cost } = req.body;
 
   await query(
-    "INSERT INTO cost_guest_sort (person_type, cost, created_at, updated_at) VALUES (?,?,?,?)",
-    [person_type, cost, created_at, updated_at]
+    "INSERT INTO cost_guest_sort (person_type, cost, created_at, updated_at) VALUES (?,?,NOW(),NOW())",
+    [person_type, cost]
   );
 
   res.status(200).send("Succesvol een nieuw soort gast en kosten aangemaakt");
 });
 
 router.post("/createAccommodations", async (req, res) => {
-  const created_at = new Date().toISOString().slice(0, 19).replace("T", " ");
-  const updated_at = created_at;
   const { accommodation_type, description_note, cost } = req.body;
 
   await query(
-    "INSERT INTO accommodations (accommodation_type, description_note, cost, created_at, updated_at) VALUES(?,?,?,?,?)",
-    [accommodation_type, description_note, cost, created_at, updated_at]
+    "INSERT INTO accommodations (accommodation_type, description_note, cost, created_at, updated_at) VALUES(?,?,?,NOW(),NOW())",
+    [accommodation_type, description_note, cost]
   );
 
   res.status(200).send("Succesvol een nieuw accomodatie type aangemaakt");
 });
 
 router.post("/createCampingSpots", async (req, res) => {
-  const created_at = new Date().toISOString().slice(0, 19).replace("T", " ");
-  const updated_at = created_at;
   const { accommodations_id, spot_name, spot_status, notes } = req.body;
 
   await query(
-    "INSERT INTO camping_spots (accommodations_id, spot_name, spot_status, notes, created_at, updated_at) VALUES(?,?,?,?,?,?)",
-    [accommodations_id, spot_name, spot_status, notes, created_at, updated_at]
+    "INSERT INTO camping_spots (accommodations_id, spot_name, spot_status, notes, created_at, updated_at) VALUES(?,?,?,?,NOW(),NOW())",
+    [accommodations_id, spot_name, spot_status, notes]
   );
 
   res.status(200).send("Succesvol een camping spot aangemaakt");
