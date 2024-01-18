a671080b5fb5b32845dac10210b4bf1fae90a205
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-18 16:24:40 +0100

    refactored the routes

--- src/RoutesProvider.tsx
+++ src/RoutesProvider.tsx
@@ -10,13 +10,7 @@ import { AuthMiddleware } from "./utils/middleware/AuthMiddleware";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route
-      path="*"
-      element={<AuthMiddleware />}
-      action={async ({ request }) => {
-        return request.headers.getSetCookie();
-      }}
-    >
+    <Route path="*" element={<AuthMiddleware />}>
       <Route path="" element={<Login />} />
       <Route path="dashboard" element={<Dashboard />} />
     </Route>
