765873e6b714c48a1638da26633b472258ed65eb
Ethan <147701278+Milk-Soda@users.noreply.github.com>
2024-01-23 12:12:38 +0100

    added temp route for testing the chatbot

--- src/RoutesProvider.tsx
+++ src/RoutesProvider.tsx
@@ -7,11 +7,14 @@ import Dashboard from "@pages/Dashboard";
 import Login from "@pages/Login";
 import { AuthMiddleware } from "./utils/middleware/AuthMiddleware";
 
+import ChatTest from "./pages/ChatTest";
+
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="*" element={<AuthMiddleware />}>
       <Route path="" element={<Login />} />
       <Route path="dashboard" element={<Dashboard />} />
+      <Route path="chat-test" element={<ChatTest />} />
     </Route>
   )
 );
