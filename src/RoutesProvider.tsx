b6b97f14f286d8e525286a629506b8c4cda8a91a
Finn Kuiper <finnkuiper05@gmail.com>
2024-01-24 21:41:15 +0100

    Add Places route to RoutesProvider

--- src/RoutesProvider.tsx
+++ src/RoutesProvider.tsx
@@ -1,17 +1,19 @@
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
 } from "react-router-dom";
 import Dashboard from "@pages/Dashboard";
 import Login from "@pages/Login";
 import { AuthMiddleware } from "./utils/middleware/AuthMiddleware";
+import Places from "@pages/Places";
 
 export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="*" element={<AuthMiddleware />}>
-      <Route path="" element={<Login />} />
-      <Route path="dashboard" element={<Dashboard />} />
-    </Route>
-  )
+    createRoutesFromElements(
+        <Route path="*" element={<AuthMiddleware />}>
+            <Route path="" element={<Login />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="plaatsen" element={<Places />} />
+        </Route>
+    )
 );
