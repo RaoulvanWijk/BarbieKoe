c1ebda273a6ac434e37a90f08d8f2450af3f015b
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-24 16:38:25 +0100

    add loading state to item content component

--- src/components/Reservation/ItemContent.tsx
+++ src/components/Reservation/ItemContent.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 type ItemContentProps = {
   title: string;
-  content: string|number;
+  content: string|number|undefined;
 };
 
 export default function ItemContent(props: ItemContentProps) {
   return (
     <div>
       <dt>{props.title}</dt>
-      <dd>{props.content}</dd>
+      {
+        props.content !== undefined ? <dd>{props.content}</dd> : <hr className="h-4 w-16 animate-pulse outline-none border-none bg-gray-300"  />
+      }
+
     </div>
   );
 }
