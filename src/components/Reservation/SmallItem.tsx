d016689b3420e6abc8baf0791c0a91d221fbd82c
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-24 16:03:17 +0100

    Add SmallItem component for displaying reservation details

---
+++ src/components/Reservation/SmallItem.tsx
@@ -0,0 +1,46 @@
+import { ReservationFetch } from "@/lib/types/database";
+import ItemContent from "./ItemContent";
+import "/resources/styles/components/reservation/item.scss";
+
+type SmallItemProps = {
+  reservation: ReservationFetch;
+};
+export default function SmallItem(props: SmallItemProps) {
+  const { reservation } = props;
+  console.log(reservation.id);
+  
+  return (
+    <div
+      key={reservation.id}
+      className="reservation-item"
+    >
+      <div className="item-header">
+        <h3 className="title">
+          {reservation.first_name} {reservation.last_name}
+        </h3>
+        <p className="sub-title">
+          {reservation.email}
+        </p>
+      </div>
+      <div className="content-container">
+        <dl className="content">
+          <ItemContent
+            title="Aankomst datum"
+            content={new Date(reservation.arrival).toLocaleString("nl-NL")}
+          />
+          <ItemContent
+            title="Vertrek datum"
+            content={new Date(reservation.departure).toLocaleString("nl-NL")}
+          />
+          <ItemContent title="Kosten" content={reservation.cost} />
+          <ItemContent
+            title="Status"
+            content={
+              reservation.booking_status == 0 ? "Niet ingecheckt" : "Ingecheckt"
+            }
+          />
+        </dl>
+      </div>
+    </div>
+  );
+}
