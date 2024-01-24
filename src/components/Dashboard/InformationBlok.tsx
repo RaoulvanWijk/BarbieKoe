2bbb0d2234224ff9d81793044d6b44fb62e9d8de
Finn Kuiper <finnkuiper05@gmail.com>
2024-01-24 21:41:35 +0100

    Add route prop to InformationBlok component

--- src/components/Dashboard/InformationBlok.tsx
+++ src/components/Dashboard/InformationBlok.tsx
@@ -5,13 +5,17 @@ import { Link } from "react-router-dom";
 type InformationBlokProps = {
     name: string;
     children: React.ReactNode;
+    route: string;
 };
 export default function InformationBlok(props: InformationBlokProps) {
     return (
         <div className="info-blok">
             <h1>{props.name}</h1>
             {props.children}
-            <Link to="/dashboard" className="text-blue-400 mt-5 info-link-text">
+            <Link
+                to={props.route}
+                className="text-blue-400 mt-5 info-link-text"
+            >
                 Meer info →
             </Link>
         </div>
