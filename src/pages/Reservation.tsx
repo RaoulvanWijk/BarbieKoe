c932182cab7c3b4ba9467d4aa59b23cdefe88587
RaoulvanWijk <raoul.van.wijk05@gmail.com>
2024-01-24 16:39:52 +0100

    added page for single reservation

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -9,10 +9,10 @@ import { ReservationFetch } from "@/lib/types/database";
 export default function Reservation() {
   const id = useParams<{ id: string }>().id
   console.log(id);
-  const reservation = useData<ReservationFetch>(`/api/booking/find/${id}`)
+  const reservation = useData<ReservationFetch[]>(`/api/booking/find/${id}`)
   return (
-    <PageLayout pageTitle={reservation?.first_name ?? "Loading..."}>
-      <LageItem reservation={reservation} />
+    <PageLayout>
+      <LageItem reservation={reservation?.[0]} />
     </PageLayout>
 
   )
